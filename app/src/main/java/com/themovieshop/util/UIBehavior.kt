393af29fb9c4e87fd3b0c4d069c99d6c30561275
John Anders Murillo <47459887+andersmurillo92@users.noreply.github.com>
2023-09-21 18:05:00 -0500

    Add needed utils when managing data

---
+++ app/src/main/java/com/themovieshop/util/UIBehavior.kt
@@ -0,0 +1,13 @@
+package com.themovieshop.util
+
+interface UIBehavior {
+    fun initUI()
+
+    interface RecyclerView{
+        fun initRecyclerView()
+    }
+
+    interface ViewPager{
+        fun initViewPager2()
+    }
+}
